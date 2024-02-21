137db3bbfafd228fad8c095c08252daa1cc97d61
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-02-21 17:05:35 +0500

    Add project files.

---
+++ EntityCoreLesson/Application/MyServices/CompanyService/CompanyService.cs
@@ -0,0 +1,104 @@
+﻿using EntityCoreLesson.Domain.DTOs;
+using EntityCoreLesson.Domain.Models;
+using EntityCoreLesson.Infastructure;
+using Microsoft.EntityFrameworkCore;
+
+namespace EntityCoreLesson.Application.MyServices.CompanyService
+{
+    public class CompanyService : ICompanyService
+    {
+        public ApplicationDbContext _context;
+        public CompanyService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<string> CreateCompanyAsync(CompanyDTO cmp)
+        {
+            try
+            {
+                var model = new Company()
+                {
+                    Name = cmp.Name,
+                    Description = cmp.Description,
+                    Employee_count = cmp.Employee_count,
+                };
+                await _context.Companys.AddAsync(model);
+                _context.SaveChanges();
+                return "Yaratildi";
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+
+        public async Task<bool> DeleteCompanyByIdAsync(int id)
+        {
+            try
+            {
+                var md = await _context.Companys.FirstOrDefaultAsync(x => x.Id == id);
+                if (md != null)
+                {
+                    _context.Companys.Remove(md);
+                    _context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<Company>> GetAllCompanysAsync()
+        {
+            try
+            {
+                var x = await _context.Companys.ToListAsync();
+                return x;
+            }
+            catch
+            {
+                return Enumerable.Empty<Company>();
+            }
+        }
+
+        public async Task<Company> GetCompanyByIdAsync(int id)
+        {
+            try
+            {
+                var md = await _context.Companys.FirstOrDefaultAsync(x => x.Id == id);
+                if (md != null)
+                {
+                    return md;
+                }
+                return new Company() { };
+            }
+            catch
+            {
+                return new Company() { };
+            }
+        }
+
+        public async Task<bool> UpdateCompanyById(int id, CompanyDTO company)
+        {
+            try
+            {
+                var md = await _context.Companys.FirstOrDefaultAsync(x => x.Id == id);
+                if (md != null)
+                {
+                    md.Name = company.Name;
+                    md.Description = company.Description;
+                    _context.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ EntityCoreLesson/Controllers/CompanyController.cs
@@ -0,0 +1,79 @@
+﻿using EntityCoreLesson.Application.MyServices.CompanyService;
+using EntityCoreLesson.Domain.DTOs;
+using EntityCoreLesson.Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityCoreLesson.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        public ICompanyService _comService;
+        public CompanyController(ICompanyService comS)
+        {
+            _comService = comS;
+        }
+        [HttpPost]
+        public async Task<string> Create(CompanyDTO cmp)
+        {
+            try
+            {
+                return await _comService.CreateCompanyAsync(cmp);
+            }
+            catch
+            {
+                return "Error";
+            }
+        }
+        [HttpGet]
+        public async Task<IEnumerable<Company>> GetAllCompaniesAsync()
+        {
+            try
+            {
+                return await _comService.GetAllCompanysAsync();
+            }
+            catch
+            {
+                return Enumerable.Empty<Company>();
+            }
+        }
+        [HttpGet]
+        public async Task<Company> GetCompanyByIdAsync(int id)
+        {
+            try
+            {
+                return await _comService.GetCompanyByIdAsync(id);
+            }
+            catch
+            {
+                return new Company() { };
+            }
+        }
+        [HttpDelete]
+        public async Task<bool> DeleteCompanyByIdAsync(int id)
+        {
+            try
+            {
+                return await _comService.DeleteCompanyByIdAsync(id);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        [HttpPut]
+        public async Task<bool> UpdateCompanyByIdAsync(int id,CompanyDTO company)
+        {
+            try
+            {
+                return await _comService.UpdateCompanyById(id, company);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
